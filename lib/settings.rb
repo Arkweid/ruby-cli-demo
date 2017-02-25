9929a77ecf32f6e3a1a7ab0ae85269cc170095ab
Julien Vanier <jvanier@gmail.com>
2017-02-25 17:29:23 -0500

    Added settings

--- lib/settings.rb
+++ lib/settings.rb
@@ -1,10 +1,28 @@
+require "yaml"
+
 class Settings
   def initialize(filename = nil)
     @filename = filename || default_filename
+    @values = {}
   end
   attr_reader :filename
 
   def default_filename
     "~/.button-cli.yaml"
+  end
+
+  def [](key)
+    @values[key.to_s]
+  end
+
+  def []=(key, value)
+    @values[key.to_s] = value
+  end
+
+  def load
+    file_content = File.open(filename, 'r').read
+    @values = YAML.load(file_content)
+  rescue Errno::ENOENT
+    # Don't worry if file is not there
   end
 end
--- spec/settings_spec.rb
+++ spec/settings_spec.rb
@@ -1,4 +1,5 @@
 require "settings"
+require "tempfile"
 
 RSpec.describe Settings do
   describe "#default_filename" do
@@ -18,6 +19,36 @@ RSpec.describe Settings do
     it "uses default_filename when no constructor parameter is passed" do
       settings = Settings.new
       expect(settings.filename).to eq settings.default_filename
+    end
+  end
+
+  describe "#[]" do
+    it "accesses values" do
+      settings = Settings.new
+      settings[:foo] = "bar"
+      expect(settings[:foo]).to eq "bar"
+    end
+  end
+
+  describe "#load" do
+    it "loads from the file" do
+      Tempfile.open('settings.yaml') do |f|
+        f.write(%q(
+                token: XXXXXXXXXXXXXXXX
+                device: button
+                ))
+        f.close
+
+        settings = Settings.new(f.path)
+        settings.load
+        expect(settings[:token]).to eq "XXXXXXXXXXXXXXXX"
+        expect(settings[:device]).to eq "button"
+      end
+    end
+
+    it "doesn't crash when the file is missing" do
+      settings = Settings.new("/missing")
+      expect { settings.load }.not_to raise_error
     end
   end
 end
